ba762d0f6044e26fd429fb42f347596595a10803
blt <bernardoluzteixeira@gmail.com>
2013-10-16 19:37:27 -0700

    fingers crossed jquery

---
+++ 37Signals.js
@@ -0,0 +1,20 @@
+$(document).ready( function() { 
+
+  $("#basecamp").hover( function() {
+    $('.main').hide();
+    $('#bc').show();
+  },
+   function () {
+    $('#bc').hide();
+    $('.main').show();
+});
+    
+
+// remove main (change display to none) -- no p2 (assign class of p2) :(
+// find bc (change display to block)
+// add bc
+// mouseleave add main back in
+
+
+});
+
