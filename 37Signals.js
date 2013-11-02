6ccc760441827c1cfaadf35dd69dc66169736bf9
blt <bernardoluzteixeira@gmail.com>
2013-11-02 00:29:38 -0700

    arrows

--- 37Signals.js
+++ 37Signals.js
@@ -3,29 +3,35 @@ $(document).ready( function() {
   $("#basecamp").hover( function() {
     $('#main').hide();
     $('#bc').show();
+    $('#right1').show();
   },
    function () {
     $('#bc').hide();
     $('#main').show();
+    $('#right1').hide();
 });
 
 
   $("#highrise").hover( function() {
     $('#main').hide();
     $('#hr').show();
+    $('#right2').show();
   },
    function () {
     $('#hr').hide();
     $('#main').show();
+    $('#right2').hide();
 });
 
   $("#campfire").hover( function() {
     $('#main').hide();
     $('#cf').show();
+    $('#left').show();
   },
    function () {
     $('#cf').hide();
     $('#main').show();
+    $('#left').hide();
 });
 
 });
